8e322cf3b3b0350f569aecf5cf7b05ab3ca05d23
Will Keaney <will.keaney@kroger.com>
2019-05-08 13:32:23 -0400

    Copy the test engine. Set one copy to run in stream mode, and the other in cloud. Copy unit tests, with one set using the stream engine and the other using the cloud engine.

--- src/test/java/org/opennms/drools/dao/test/DroolsNodeDaoTest.java
+++ src/test/java/org/opennms/drools/dao/test/DroolsNodeDaoTest.java
@@ -1,7 +1,6 @@
 package org.opennms.drools.dao.test;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.opennms.core.soa.ServiceRegistry;
 import org.opennms.core.spring.BeanUtils;
@@ -14,7 +13,6 @@ import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.events.EventBuilder;
 import org.opennms.netmgt.xml.event.Event;
-import org.opennms.netmgt.xml.event.Parm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,14 +41,16 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
         return m_serviceRegistry;
     }
 
-    private DroolsCorrelationEngine m_engine;
+    private DroolsCorrelationEngine m_engine_cloud;
+    private DroolsCorrelationEngine m_engine_stream;
 
     @Before
     @BeforeTransaction
     @SuppressWarnings("Duplicates")
     public void setUp() {
         getAnticipator().reset();
-        m_engine = findEngineByName("dao-test");
+        m_engine_cloud = findEngineByName("dao-test-cloud");
+        m_engine_stream = findEngineByName("dao-test-stream");
         try {
             if (!m_populated) {
                 m_databasePopulator.populateDatabase();
@@ -64,7 +64,7 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
     @Test
     @Transactional
-    public void testIdentifyNodeByLabel() {
+    public void testIdentifyNodeByLabelCloudMode() {
         OnmsNode node = m_nodeDao.get(1);
 
         Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
@@ -74,13 +74,13 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
         m_anticipatedMemorySize = 0;
         anticipate(anticipated);
-        m_engine.correlate(event);
-        verify(m_engine);
+        m_engine_cloud.correlate(event);
+        verify(m_engine_cloud);
     }
 
     @Test
     @Transactional
-    public void testIdentifyNodeByForeignIdWithSimpleDao() {
+    public void testIdentifyNodeByForeignIdWithSimpleDaoCloudMode() {
         OnmsNode node = m_nodeDao.get(2);
         node.setForeignSource("unit-test-foreign-source");
         node.setForeignId(node.getLabel());
@@ -97,13 +97,13 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
         m_anticipatedMemorySize = 0;
         anticipate(anticipated);
-        m_engine.correlate(event);
-        verify(m_engine);
+        m_engine_cloud.correlate(event);
+        verify(m_engine_cloud);
     }
 
     @Test
     @Transactional
-    public void testIdentifyNodeByForeignIdWithDatabasePopulatorDao() {
+    public void testIdentifyNodeByForeignIdWithDatabasePopulatorDaoCloudMode() {
         OnmsNode node = m_nodeDao.get(3);
         node.setForeignSource("unit-test-foreign-source");
         node.setForeignId(node.getLabel());
@@ -120,17 +120,17 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
         m_anticipatedMemorySize = 0;
         anticipate(anticipated);
-        m_engine.correlate(event);
-        verify(m_engine);
+        m_engine_cloud.correlate(event);
+        verify(m_engine_cloud);
     }
 
     @Test
     @Transactional
-    public void testIdentifyNodeByForeignIdWithKieSessionDao() {
+    public void testIdentifyNodeByForeignIdWithKieSessionDaoCloudMode() {
         OnmsNode node = m_nodeDao.get(4);
         node.setForeignSource("unit-test-foreign-source");
         node.setForeignId(node.getLabel());
-        NodeDao sessionNodeDao = (NodeDao)m_engine.getKieSession().getGlobal("nodeDao");
+        NodeDao sessionNodeDao = (NodeDao)m_engine_cloud.getKieSession().getGlobal("nodeDao");
         sessionNodeDao.saveOrUpdate(node);
         sessionNodeDao.flush();
 
@@ -144,18 +144,18 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
         m_anticipatedMemorySize = 0;
         anticipate(anticipated);
-        m_engine.correlate(event);
-        verify(m_engine);
+        m_engine_cloud.correlate(event);
+        verify(m_engine_cloud);
     }
 
     @Test
     @Transactional
-    public void testCreateNewOnmsNodeAndCorrelateEventWithIt() {
+    public void testCreateNewOnmsNodeAndCorrelateEventWithItCloudMode() {
         OnmsNode node = new OnmsNode(m_databasePopulator.getMonitoringLocationDao().getDefaultLocation(),
                                      "unit-test-foreign-id-new-node");
         node.setForeignSource("unit-test-foreign-source");
         node.setForeignId("unit-test-foreign-id-new-node");
-        NodeDao sessionNodeDao = (NodeDao)m_engine.getKieSession().getGlobal("nodeDao");
+        NodeDao sessionNodeDao = (NodeDao)m_engine_cloud.getKieSession().getGlobal("nodeDao");
         sessionNodeDao.saveOrUpdate(node);
         sessionNodeDao.flush();
 
@@ -169,7 +169,118 @@ public class DroolsNodeDaoTest extends CorrelationRulesTestCase {
 
         m_anticipatedMemorySize = 0;
         anticipate(anticipated);
-        m_engine.correlate(event);
-        verify(m_engine);
+        m_engine_cloud.correlate(event);
+        verify(m_engine_cloud);
+    }
+
+    @Test
+    @Transactional
+    public void testIdentifyNodeByLabelStreamMode() {
+        OnmsNode node = m_nodeDao.get(1);
+
+        Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
+                .setNodeid(0L).setHost(node.getLabel()).addParam("findByNodeLabel","true").getEvent();
+        Event anticipated = JaxbUtils.unmarshal(Event.class,JaxbUtils.marshal(event));
+        anticipated.setNodeid(node.getId().longValue());
+
+        m_anticipatedMemorySize = 0;
+        anticipate(anticipated);
+        m_engine_stream.correlate(event);
+        verify(m_engine_stream);
+    }
+
+    @Test
+    @Transactional
+    public void testIdentifyNodeByForeignIdWithSimpleDaoStreamMode() {
+        OnmsNode node = m_nodeDao.get(2);
+        node.setForeignSource("unit-test-foreign-source");
+        node.setForeignId(node.getLabel());
+        m_nodeDao.saveOrUpdate(node);
+        m_nodeDao.flush();
+
+        Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
+                .setNodeid(0L).setHost(node.getLabel()).addParam("findByForeignId","true").getEvent();
+        Event anticipated = JaxbUtils.unmarshal(Event.class,JaxbUtils.marshal(event));
+        anticipated.setNodeid(node.getId().longValue());
+
+        OnmsNode nodeFoundByDao = m_nodeDao.findByForeignId("unit-test-foreign-source",node.getLabel());
+        assertEquals(node.getId(),nodeFoundByDao.getId());
+
+        m_anticipatedMemorySize = 0;
+        anticipate(anticipated);
+        m_engine_stream.correlate(event);
+        verify(m_engine_stream);
+    }
+
+    @Test
+    @Transactional
+    public void testIdentifyNodeByForeignIdWithDatabasePopulatorDaoStreamMode() {
+        OnmsNode node = m_nodeDao.get(3);
+        node.setForeignSource("unit-test-foreign-source");
+        node.setForeignId(node.getLabel());
+        m_databasePopulator.getNodeDao().saveOrUpdate(node);
+        m_databasePopulator.getNodeDao().flush();
+
+        Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
+                .setNodeid(0L).setHost(node.getLabel()).addParam("findByForeignId","true").getEvent();
+        Event anticipated = JaxbUtils.unmarshal(Event.class,JaxbUtils.marshal(event));
+        anticipated.setNodeid(node.getId().longValue());
+
+        OnmsNode nodeFoundByDao = m_nodeDao.findByForeignId("unit-test-foreign-source",node.getLabel());
+        assertEquals(node.getId(),nodeFoundByDao.getId());
+
+        m_anticipatedMemorySize = 0;
+        anticipate(anticipated);
+        m_engine_stream.correlate(event);
+        verify(m_engine_stream);
+    }
+
+    @Test
+    @Transactional
+    public void testIdentifyNodeByForeignIdWithKieSessionDaoStreamMode() {
+        OnmsNode node = m_nodeDao.get(4);
+        node.setForeignSource("unit-test-foreign-source");
+        node.setForeignId(node.getLabel());
+        NodeDao sessionNodeDao = (NodeDao)m_engine_stream.getKieSession().getGlobal("nodeDao");
+        sessionNodeDao.saveOrUpdate(node);
+        sessionNodeDao.flush();
+
+        Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
+                .setNodeid(0L).setHost(node.getLabel()).addParam("findByForeignId","true").getEvent();
+        Event anticipated = JaxbUtils.unmarshal(Event.class,JaxbUtils.marshal(event));
+        anticipated.setNodeid(node.getId().longValue());
+
+        OnmsNode nodeFoundByDao = m_nodeDao.findByForeignId("unit-test-foreign-source",node.getLabel());
+        assertEquals(node.getId(),nodeFoundByDao.getId());
+
+        m_anticipatedMemorySize = 0;
+        anticipate(anticipated);
+        m_engine_stream.correlate(event);
+        verify(m_engine_stream);
+    }
+
+    @Test
+    @Transactional
+    public void testCreateNewOnmsNodeAndCorrelateEventWithItStreamMode() {
+        OnmsNode node = new OnmsNode(m_databasePopulator.getMonitoringLocationDao().getDefaultLocation(),
+                                     "unit-test-foreign-id-new-node");
+        node.setForeignSource("unit-test-foreign-source");
+        node.setForeignId("unit-test-foreign-id-new-node");
+        NodeDao sessionNodeDao = (NodeDao)m_engine_stream.getKieSession().getGlobal("nodeDao");
+        sessionNodeDao.saveOrUpdate(node);
+        sessionNodeDao.flush();
+
+        Event event = new EventBuilder("uei.opennms.org/test/droolsDaoTestEvent","tests")
+                .setNodeid(0L).setHost(node.getLabel()).addParam("findByForeignId","true").getEvent();
+        Event anticipated = JaxbUtils.unmarshal(Event.class,JaxbUtils.marshal(event));
+        anticipated.setNodeid(node.getId().longValue());
+
+        OnmsNode nodeFoundByDao = m_nodeDao.findByForeignId("unit-test-foreign-source","unit-test-foreign-id-new-node");
+        assertEquals(node.getId(),nodeFoundByDao.getId());
+
+        m_anticipatedMemorySize = 0;
+        anticipate(anticipated);
+        m_engine_stream.correlate(event);
+        verify(m_engine_stream);
     }
 }
